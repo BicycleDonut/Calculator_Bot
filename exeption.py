c048e134ab03780e6415d944f1849346664277da
Alexander Ryzhov <aryzhov84@gmail.com>
2022-11-22 00:44:53 +0300

    add exeption.py

---
+++ exeption.py
@@ -0,0 +1,14 @@
+def check_num(line):
+    return True if line.isdigit() or line.replace(".", "", 1).isdigit() else False
+
+
+def check_exit(line):
+    return True if line == 'q' else False
+
+
+def check_back(line):
+    return True if line == 'r' else False
+
+
+def check_comp(line):
+    return True if line in ['1', '2', '3', '4'] else False
